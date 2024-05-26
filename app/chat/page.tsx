55524de7ddb437755d5373ba71483946d7705fd3
NiladriChatterje <radongtxgpu@gmail.com>
2024-05-26 18:31:53 +0530

    package.json has a missing vital component (fixed)

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,6 +1,21 @@
 'use client'
-const page = (e: { searchParams: { url: string } }) => {
-    console.log(e.searchParams.url)
+import { CheerioWebBaseLoader } from '@langchain/community/document_loaders/web/cheerio'
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { useRef } from 'react'
+
+
+const page = async (formData: { searchParams: { url: string } }) => {
+    const { searchParams: { url } } = formData;
+
+    const loader = new CheerioWebBaseLoader(url);
+
+    const docs = await loader.load();
+
+    const splitter = new RecursiveCharacterTextSplitter();
+    const splitDocs = await splitter.splitDocuments(docs);
+
+    console.log(splitDocs)
+
     return (
         <div className="flex min-h-screen flex-col items-center justify-center p-24">
             <div className="absolute z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]" />
